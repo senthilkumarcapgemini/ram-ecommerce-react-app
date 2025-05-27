4bd2c3c0a1c249cb633835d69531d2ddbfce7388
ramkumar-88 <ram.nagarajan88@gmail.com>
2025-05-27 18:07:18 +0530

    changes..

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore, combineReducers } from 'redux';
+import {configureStore,getDefaultMiddleware } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from '@redux-devtools/extension'
 
 const initialState = {
     user: null,
@@ -26,6 +28,12 @@ const rootReducer = combineReducers({
     user: userReducer,
 });
 
-const store = createStore(rootReducer);
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(/* your custom middleware */),
+        devTools: process.env.NODE_ENV !== 'production'
+        
+});
 
 export default store;
